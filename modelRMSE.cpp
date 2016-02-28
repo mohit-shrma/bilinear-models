a6528444f859b48549eaacd126f69cd6449ac95b
mohit sharma <sharm163@umn.edu>
2016-02-27 19:56:15 -0600

    added sparse lazy updates

--- modelRMSE.cpp
+++ modelRMSE.cpp
@@ -58,27 +58,42 @@ void ModelRMSE::computeRMSEGrad(Eigen::VectorXf& uFeat, Eigen::VectorXf& iFeat, 
 
 
 void ModelRMSE::computeRMSESparseGrad(int u, int i, float r_ui, 
-    Eigen::MatrixXf& Wgrad, Eigen::VectorXf& pdt, const Data& data) {
+    Eigen::MatrixXf& Wgrad, Eigen::VectorXf& pdt, const Data& data,
+    std::chrono::time_point<std::chrono::system_clock>& startRat,
+    std::chrono::time_point<std::chrono::system_clock>& endRat,
+    std::chrono::time_point<std::chrono::system_clock>& startOut,
+    std::chrono::time_point<std::chrono::system_clock>& endOut,
+    std::chrono::time_point<std::chrono::system_clock>& startUpdGrad,
+    std::chrono::time_point<std::chrono::system_clock>& endUpdGrad) {
   
   float r_ui_est;
-   
-  r_ui_est  = estPosRating(u, i, data, pdt);
-   
-  Wgrad.fill(0);
+  float l2RegTwice = 2.0*l2Reg;
 
+  startRat = std::chrono::system_clock::now();
+  r_ui_est  = estPosRating(u, i, data, pdt);
+  endRat = std::chrono::system_clock::now();
+  
+  startUpdGrad = std::chrono::system_clock::now();
+  Wgrad = l2RegTwice*W;
+  endUpdGrad = std::chrono::system_clock::now();
+
+  float outWt = 2.0*(r_ui_est - r_ui); 
+
+  startOut = std::chrono::system_clock::now();
   //f_u*f_i^T
-  updateMatWSpOuterPdt(Wgrad, data.uFAccumMat, u, data.itemFeatMat, i, 1);
+  updateMatWSpOuterPdt(Wgrad, data.uFAccumMat, u, data.itemFeatMat, i, outWt);
   
   //-f_i*f_i^T
-  updateMatWSpOuterPdt(Wgrad, data.itemFeatMat, i, data.itemFeatMat, i, -1);
-  
+  updateMatWSpOuterPdt(Wgrad, data.itemFeatMat, i, data.itemFeatMat, i, -1*outWt);
+  endOut = std::chrono::system_clock::now();
+
   //2*(r_ui_est-r_ui)
-  Wgrad *= 2.0*(r_ui_est - r_ui);
+  //Wgrad *= 2.0*(r_ui_est - r_ui);
     
   //add Wgrad to gradient of l2 reg
-  Wgrad += 2.0*l2Reg*W;
-
+  //Wgrad += 2.0*l2Reg*W;
 }
+
 
 //gradient check
 void ModelRMSE::gradCheck(Eigen::VectorXf& uFeat, Eigen::VectorXf& iFeat, 
@@ -120,13 +135,14 @@ void ModelRMSE::gradCheck(Eigen::VectorXf& uFeat, Eigen::VectorXf& iFeat,
 
 }
 
-
-void ModelRMSE::train(const Data &data, Model& bestModel) {
+/*
+void ModelRMSE::train2(const Data &data, Model& bestModel) {
   
   std::cout << "\nModelRMSE::train";
 
   int bestIter;
   Eigen::MatrixXf Wgrad(nFeatures, nFeatures);  
+  Eigen::MatrixXf::Zero T(nFeatures, nFeatures);
   Eigen::VectorXf iFeat(nFeatures);
   Eigen::VectorXf uFeat(nFeatures);
   Eigen::VectorXf pdt(nFeatures);
@@ -138,9 +154,21 @@ void ModelRMSE::train(const Data &data, Model& bestModel) {
    
   std::cout <<"\nB4 Train Objective: " << objective(data) << std::endl;
 
+  std::chrono::time_point<std::chrono::system_clock> startSub, endSub;
+  std::chrono::time_point<std::chrono::system_clock> startGrad, endGrad;
+  std::chrono::time_point<std::chrono::system_clock> startRat, endRat;
+  std::chrono::time_point<std::chrono::system_clock> startOut, endOut;
+  std::chrono::time_point<std::chrono::system_clock> startUpdGrad, endUpdGrad;
+  std::chrono::duration<double> durationGrad, durationRat;
+  double gradDuration = 0;
+  double ratDuration = 0;
+  double outDuration, updDuration;
   for (int iter = 0; iter < maxIter; iter++) {
-    std::chrono::time_point<std::chrono::system_clock> startSub, endSub;
     startSub = std::chrono::system_clock::now();
+    gradDuration = 0;
+    ratDuration = 0;
+    outDuration = 0;
+    updDuration = 0;
     for (int subIter = 0; subIter < trainNNZ; subIter++) {
     //for (int subIter = 0; subIter < 10; subIter++) {
       
@@ -161,22 +189,43 @@ void ModelRMSE::train(const Data &data, Model& bestModel) {
       
       //extractFeat(data.uFAccumMat, u, uFeat);
       //extractFeat(data.itemFeatMat, item, iFeat);
-
+      
+      startGrad = std::chrono::system_clock::now();
       //compute gradient
       //gradCheck(uFeat, iFeat, Wgrad, r_ui);
       //computeRMSEGrad(uFeat, iFeat, Wgrad, r_ui);
-      computeRMSESparseGrad(u, item, r_ui, Wgrad, pdt, data);
+      
+      computeRMSESparseGrad(u, item, r_ui, Wgrad, pdt, data, startRat, endRat,
+          startOut, endOut, startUpdGrad, endUpdGrad);
+      
+      endGrad = std::chrono::system_clock::now();
+      durationGrad = endGrad - startGrad;
+      gradDuration += durationGrad.count();
+
+      durationRat = endRat - startRat;
+      ratDuration += durationRat.count();
+
+      durationRat = endOut - startOut;
+      outDuration += durationRat.count();
+
+      durationRat = endUpdGrad - startUpdGrad;
+      updDuration += durationRat.count();
 
       //update W
       W -= learnRate*Wgrad;      
+      
 
-      //std::cout << "\n" << W.block<3,3>(0,0) << std::endl;
+        //std::cout << "\n" << W.block<3,3>(0,0) << std::endl;
       //std::cout << W << std::endl;
     }
     endSub = std::chrono::system_clock::now(); 
     std::chrono::duration<double> durationSub =  (endSub - startSub) ;
-    //std::cout << "\nduration: " << durationSub.count();
-      
+    std::cout << "\nsubiter duration: " << durationSub.count();
+    std::cout << "\ngradient duration: " << gradDuration;
+    std::cout << "\nrating est duration: " << ratDuration; 
+    std::cout << "\nout duration: " << outDuration;
+    std::cout << "\nupd duration: " << updDuration << std::endl;
+
     //nuclear norm projection after all sub-iters
     std::chrono::time_point<std::chrono::system_clock> startSVD, endSVD;
     startSVD = std::chrono::system_clock::now();
@@ -186,7 +235,7 @@ void ModelRMSE::train(const Data &data, Model& bestModel) {
 
     endSVD = std::chrono::system_clock::now(); 
     std::chrono::duration<double> durationSVD =  (endSVD - startSVD) ;
-    //std::cout << "\nsvd duration: " << durationSVD.count();
+    std::cout << "\nsvd duration: " << durationSVD.count();
     
     //perform model evaluation on validation set
     if (iter %OBJ_ITER == 0) {
@@ -203,4 +252,157 @@ void ModelRMSE::train(const Data &data, Model& bestModel) {
   }
 
 }
+*/
 
+void ModelRMSE::train(const Data &data, Model& bestModel) {
+  
+  std::cout << "\nModelRMSE::train";
+
+  int bestIter;
+  Eigen::MatrixXf Wgrad(nFeatures, nFeatures);  
+  Eigen::MatrixXf T(nFeatures, nFeatures);
+  Eigen::VectorXf iFeat(nFeatures);
+  Eigen::VectorXf uFeat(nFeatures);
+  Eigen::VectorXf pdt(nFeatures);
+  float bestObj, prevObj, valRecall;
+  int trainNNZ = getNNZ(data.trainMat); 
+
+  int u, ii, item, nUserItems;
+  float r_ui;
+  float r_ui_est; 
+  std::cout <<"\nB4 Train Objective: " << objective(data) << std::endl;
+
+  std::chrono::time_point<std::chrono::system_clock> startSub, endSub;
+  std::chrono::time_point<std::chrono::system_clock> startGrad, endGrad;
+  std::chrono::time_point<std::chrono::system_clock> startRat, endRat;
+  std::chrono::time_point<std::chrono::system_clock> startOut, endOut;
+  std::chrono::time_point<std::chrono::system_clock> startUpdGrad, endUpdGrad;
+  std::chrono::duration<double> durationGrad, durationRat;
+  double gradDuration = 0;
+  double ratDuration = 0;
+  double outDuration, updDuration;
+  for (int iter = 0; iter < maxIter; iter++) {
+    startSub = std::chrono::system_clock::now();
+    gradDuration = 0;
+    ratDuration = 0;
+    outDuration = 0;
+    updDuration = 0;
+    T.fill(0);
+    int subIter;
+    for (subIter = 0; subIter < trainNNZ; subIter++) {
+    //for (int subIter = 0; subIter < 10; subIter++) {
+      
+      //sample user
+      while (1) {
+        u = std::rand() % data.nUsers;
+        if (data.posTrainUsers.find(u) != data.posTrainUsers.end()) {
+          //found u
+          break;
+        }
+      }
+      
+      //sample a rated item
+      nUserItems = data.trainMat->rowptr[u+1] - data.trainMat->rowptr[u];
+      ii = std::rand()%nUserItems + data.trainMat->rowptr[u];
+      item = data.trainMat->rowind[ii];
+      r_ui = data.trainMat->rowval[ii];
+      r_ui_est = estPosRating(u, item, data, pdt);
+        
+      float err = 2.0*(r_ui_est - r_ui);
+
+      //- learnRate * err * f_u * f_i^T
+      for (int ii2 = data.itemFeatMat->rowptr[item]; 
+          ii2 < data.itemFeatMat->rowptr[item+1]; ii2++) {
+        int ind2 = data.itemFeatMat->rowind[ii2];
+        float val2 = data.itemFeatMat->rowval[ii2];
+        for (int ii1 = data.uFAccumMat->rowptr[u];
+            ii1 < data.uFAccumMat->rowptr[u+1];
+            ii1++) {
+          int ind1 = data.uFAccumMat->rowind[ii1];
+          float val1 = data.uFAccumMat->rowval[ii1];
+          //update W(ind1, ind2)
+
+          //update with reg updates
+          W(ind1, ind2) = W(ind1, ind2)*pow((1.0 - 2*learnRate*l2Reg), 
+                                            (subIter+1)-T(ind1, ind2));
+
+          //record that reg update was done in this iter
+          T(ind1, ind2) = subIter + 1;
+
+          //do the actual update
+          W(ind1, ind2) = W(ind1, ind2) - learnRate*err*val1*val2;
+        }
+      }
+
+
+      //learnRate * err * f_i * f_i^T
+      for (int ii2 = data.itemFeatMat->rowptr[item];
+          ii2 < data.itemFeatMat->rowptr[item+1]; ii2++) {
+        int ind2 = data.itemFeatMat->rowind[ii2];
+        float val2 = data.itemFeatMat->rowval[ii2];
+        for (int ii1 = data.itemFeatMat->rowptr[item];
+            ii1 < data.itemFeatMat->rowptr[item+1];
+            ii1++) {
+          int ind1 = data.itemFeatMat->rowind[ii1];
+          float val1 = data.itemFeatMat->rowval[ii1];
+          //update W(ind1, ind2)
+
+          //update with reg updates
+          W(ind1, ind2) = W(ind1, ind2)*pow((1.0 - 2*learnRate*l2Reg), 
+                                            (subIter+1)-T(ind1, ind2));
+          
+          //record that reg update was done in this iter
+          T(ind1, ind2) = subIter + 1;
+
+          //do the actual update
+          W(ind1, ind2) = W(ind1, ind2) + learnRate*err*val1*val2;
+        }
+      }
+
+    }
+
+    //perform reg updates on all the pairs
+    for (int ind1 = 0; ind1 < nFeatures; ind1++) {
+      for (int ind2 = 0; ind2 < nFeatures; ind2++) {
+        //update with reg updates
+         W(ind1, ind2) = W(ind1, ind2)*pow((1.0 - 2*learnRate*l2Reg), 
+                                           subIter-T(ind1, ind2));
+      }
+    }
+    
+    endSub = std::chrono::system_clock::now(); 
+    std::chrono::duration<double> durationSub =  (endSub - startSub) ;
+    std::cout << "\nsubiter duration: " << durationSub.count() << std::endl;
+    /*
+    std::cout << "\ngradient duration: " << gradDuration;
+    std::cout << "\nrating est duration: " << ratDuration; 
+    std::cout << "\nout duration: " << outDuration;
+    std::cout << "\nupd duration: " << updDuration << std::endl;
+    */
+
+    //nuclear norm projection after all sub-iters
+    std::chrono::time_point<std::chrono::system_clock> startSVD, endSVD;
+    startSVD = std::chrono::system_clock::now();
+    
+    //performNucNormProjSVDLib(W, rank);
+    //performNucNormProjSVDLibWReg(W, nucReg);
+
+    endSVD = std::chrono::system_clock::now(); 
+    std::chrono::duration<double> durationSVD =  (endSVD - startSVD) ;
+    std::cout << "\nsvd duration: " << durationSVD.count();
+    
+    //perform model evaluation on validation set
+    if (iter %OBJ_ITER == 0) {
+      //valRecall = computeRecallPar(data.valMat, data, 10, data.valItems);
+      if (isTerminateModelObj(bestModel, data, iter, bestIter, bestObj, 
+          prevObj)) {
+        break;
+      }
+      std::cout << "\nIter: " << iter << " obj: " << prevObj
+        << " best iter: " << bestIter << " best obj: " << bestObj;
+      std::cout << "\nTrain RMSE: " << computeRMSE(data.trainMat, data) << std::endl;
+    }
+
+  }
+
+}
