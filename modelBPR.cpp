fe13db7c226858cf468c60fa0576b4e948d7e71e
mohit sharma <sharm163@umn.edu>
2016-03-08 16:12:53 -0600

    fixed recall call

--- modelBPR.cpp
+++ modelBPR.cpp
@@ -126,7 +126,7 @@ void ModelBPR::train(const Data &data, Model& bestModel) {
   std::cout << "\ntrain nnz: " << trainNNZ << " trainSamples: " << trainNNZ*pcSamples << std::endl;
   
   start = std::chrono::system_clock::now();
-  std::cout << "val recall: " << computeRecallPar(data.valMat, data, 10, data.valItems) << std::endl;
+  std::cout << "val recall: " << computeRecallParVec(data.valMat, data, 10, data.valItems) << std::endl;
   end = std::chrono::system_clock::now();
   std::chrono::duration<double> duration = end - start;
   std::cout << "\nValidation recall duratin: " << duration.count() << std::endl;
